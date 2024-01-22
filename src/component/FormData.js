1ed48e39d0b0a23b63ba5e6a099b09930d3fe45c
Ragini <raginiv743@gmail.com>
2024-01-22 21:11:06 +0530

    fix css

--- src/component/FormData.js
+++ src/component/FormData.js
@@ -1,34 +1,67 @@
-import React from 'react'
+import React from "react";
 
-const FormData = ({movieName}) => {
+const FormData = ({ movieName, setIsForm }) => {
+  const name = localStorage.getItem("name") || "";
+  const email = localStorage.getItem("email") || "";
+  const phone = localStorage.getItem("phone") || "";
+
   return (
-    <div className='border-2  mx-auto shadow-lg py-4 sm:px-10 '>
-        <div className='shadow-lg text-[1rem] font-bold pb-2'>
-            <h1 className='text-center'>Movie Name : {movieName}</h1>
+    <div className="border-2  mx-auto shadow-lg py-4 sm:px-10 ">
+      <div className="shadow-lg text-[1rem] font-bold pb-2">
+        <h1 className="text-center">Movie Name : {movieName}</h1>
+      </div>
+      <div className="">
+        <div className="border-gray-50 p-2 pt-4 flex flex-col sm:flex-row  sm:items-center pl-4 border-2 shadow-lg rounded-lg">
+          <label>Name : </label>
+          <input
+            type="text"
+            placeholder="enter your name"
+            className="outline-none  rounded-lg py-1 ml-4"
+            defaultValue={name}
+            onChange={(event) => {
+                localStorage.setItem("name", event.target.value);
+            }}
+          ></input>
         </div>
-        <div className=''>
-        <div className='border-gray-50 p-2 pt-4 flex flex-col sm:flex-row  sm:items-center pl-4 border-2 shadow-lg rounded-lg'>
-        <label>Name : </label>
-        <input type='text'placeholder='enter your name' className='outline-none  rounded-lg py-1 ml-4'></input>
+        <div className="border-gray-50 p-2 pt-4 flex flex-col pl-4 sm:flex-row sm:items-center border-2 shadow-lg rounded-lg">
+          <label>Phone no : </label>
+          <input
+            type="number"
+            placeholder="enter your phone numer"
+            className="outline-none  rounded-lg py-1 ml-4"
+            defaultValue={phone}
+            onChange={(event) => {
+                localStorage.setItem("phone", event.target.value);
+            }}
+          ></input>
         </div>
-        <div className='border-gray-50 p-2 pt-4 flex flex-col pl-4 sm:flex-row sm:items-center border-2 shadow-lg rounded-lg'>
-        <label>Phone no : </label>
-        <input type='number'placeholder='enter your phone numer' className='outline-none  rounded-lg py-1 ml-4'></input>
+        <div className="border-gray-50 p-2 pt-4  pl-4 flex flex-col sm:flex-row sm:items-center border-2 shadow-lg rounded-lg">
+          <label>Email: </label>
+          <input
+            type="email"
+            placeholder="enter your phone email"
+            className="outline-none rounded-lg py-1 ml-4"
+            defaultValue={email}
+            onChange={(event) => {
+                localStorage.setItem("email", event.target.value);
+            }}
+          ></input>
         </div>
-        <div className='border-gray-50 p-2 pt-4  pl-4 flex flex-col sm:flex-row sm:items-center border-2 shadow-lg rounded-lg'>
-        <label>Email: </label>
-        <input type='email'placeholder='enter your phone email' className='outline-none rounded-lg py-1 ml-4'></input>
+        <div className=" my-5">
+          <button
+            className="bg-blue-950 text-white px-4 py-1 rounded-lg md:px-8 md:py-2"
+            type="submit"
+            onClick={() => {
+                alert("Your booking successfully created");
+                setIsForm(false)
+            }}
+          >
+            Book
+          </button>
         </div>
-        <div className=' my-5'>
-            <button className='bg-blue-950 text-white px-4 py-1 rounded-lg md:px-8 md:py-2' type='submit'>Book</button>
-        </div>
-        </div>
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FormData
-
-
-
+export default FormData;
--- src/component/MovieSummary.js
+++ src/component/MovieSummary.js
@@ -1,62 +1,84 @@
-import React, { useEffect, useState } from 'react'
-import {  Form, Link, useParams } from 'react-router-dom' ;
-import FormData from './FormData.js';
+import React, { useEffect, useState } from "react";
+import { Form, Link, useParams } from "react-router-dom";
+import FormData from "./FormData.js";
 
 const MovieSummary = () => {
-    const [summarys,setSummary] = useState(null)
-    const {movieId} = useParams() ;
-   const [isForm,setIsForm] =useState(false)
-    console.log("https://api.tvmaze.com/shows/"+movieId)
+  const [summarys, setSummary] = useState(null);
+  const { movieId } = useParams();
+  const [isForm, setIsForm] = useState(false);
+  console.log("https://api.tvmaze.com/shows/" + movieId);
 
-    const getData = async() =>{
-        const data = await fetch(`https://api.tvmaze.com/shows/${movieId}`)
-        const json = await data.json()
-        console.log(json)
-        setSummary(json)
+  const getData = async () => {
+    const data = await fetch(`https://api.tvmaze.com/shows/${movieId}`);
+    const json = await data.json();
+    console.log(json);
+    setSummary(json);
+  };
 
-    };
-   
-    useEffect(() =>{
-        getData()
-    },[]) ;
+  useEffect(() => {
+    getData();
+  }, []);
 
-    if(summarys===null){return "Loading"} ;
-    const {image,name,language,rating ,status ,summary,url} = summarys ;
+  if (summarys === null) {
+    return "Loading";
+  }
+  const { image, name, language, rating, status, summary, url } = summarys;
 
   return (
-    <div className='py-8 relative lg:h-[100vh]'>
-        <div className='w-[80%] mx-auto sm:w-[70%] md:w-[80%] lg:flex lg:justify-between'>
-       <div className='w-[40%] sm:[60%] md:[70%]  mx-auto lg:w-[35%] lg:mr-[15%]'>
-        <img src ={image.medium} alt='' className='w-full'></img>
-       </div>
-       <div className='lg:w-[50%]'>
-       <div className='my-4'>
-        <h1 className='text-lg font-bold'>Name: {name}</h1>
-        <p><b>lang :</b> {language}</p>
-        <p><b>rating:</b> {rating.average}</p>
-        <p> <b>status :</b> {status}</p>
-       <p className='text-xs leading-5 md:text-sm'> <b>Description</b> : {summary}</p>
+    <div className="py-8 relative lg:h-[100vh]">
+      <div className="w-[80%] mx-auto sm:w-[70%] md:w-[80%] lg:flex lg:justify-between">
+        <div className="w-[40%] sm:[60%] md:[70%]  mx-auto lg:w-[35%] lg:mr-[15%]">
+          <img src={image.medium} alt="" className="w-full"></img>
+        </div>
+        <div className="lg:w-[50%]">
+          <div className="my-4">
+            <h1 className="text-lg font-bold">Name: {name}</h1>
+            <p>
+              <b>lang :</b> {language}
+            </p>
+            <p>
+              <b>rating:</b> {rating.average}
+            </p>
+            <p>
+              {" "}
+              <b>status :</b> {status}
+            </p>
+            
+            <p className="text-xs leading-5 md:text-sm">
+              <b>Description</b> : <span dangerouslySetInnerHTML={{__html: summary}} />
+            </p>
+          </div>
+          <div className="rounded-lg text-center">
+            <button className="px-3 py-1 bg-blue-950 text-white rounded-lg">
+              <Link to={url}> Go to website</Link>
+            </button>
+          </div>
+          {!isForm && (
+            <div
+              className="rounded-lg text-center my-4"
+              onClick={() => {
+                setIsForm(!isForm);
+              }}
+            >
+              <button className="px-3 py-1 bg-blue-950 text-white rounded-lg">
+                {" "}
+                book
+              </button>
+            </div>
+          )}
 
-       </div>
-       <div className='rounded-lg text-center'>
-            <button className='px-3 py-1 bg-blue-950 text-white rounded-lg'><Link to ={url}> Go to website</Link></button>
-       </div>
-       <div className='rounded-lg text-center my-4' onClick={() =>{
-        setIsForm(!isForm)
-       }}>
-            <button className='px-3 py-1 bg-blue-950 text-white rounded-lg'> book</button>
-       </div>
-       <div className=' lg:w-[100%] mx-auto '>
-       {isForm?<FormData movieName={name} />:null}
-       </div>
-       </div>
-       </div>
-       {/* <div className='lg:pt-20 lg:w-[50%] mx-auto '>
+          {isForm && (
+            <div className=" lg:w-[100%] mx-auto ">
+              {isForm ? <FormData movieName={name} setIsForm={setIsForm}/> : null}
+            </div>
+          )}
+        </div>
+      </div>
+      {/* <div className='lg:pt-20 lg:w-[50%] mx-auto '>
        {isForm?<FormData movieName={name} />:null}
        </div> */}
-    
     </div>
-  )
-}
+  );
+};
 
-export default MovieSummary
+export default MovieSummary;
